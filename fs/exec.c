a3e12d247f3534d29e185816169f7ca73d460ad2
Tom Marshall <tdm.code@gmail.com>
2017-01-25 18:01:03 +0100

    kernel: Only expose su when daemon is running
    It has been claimed that the PG implementation of 'su' has security
    vulnerabilities even when disabled.  Unfortunately, the people that
    find these vulnerabilities often like to keep them private so they
    can profit from exploits while leaving users exposed to malicious
    hackers.
    
    In order to reduce the attack surface for vulnerabilites, it is
    therefore necessary to make 'su' completely inaccessible when it
    is not in use (except by the root and system users).
    
    Change-Id: I79716c72f74d0b7af34ec3a8054896c6559a181d
    (cherry picked from commit 853e06c28f8655040d8c00f0ee2f1d72a1d8dc14)
    Signed-off-by: Laster K. (lazerl0rd) <officiallazerl0rd@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1600,6 +1600,11 @@ static int do_execve_common(const char *filename,
 	if (retval < 0)
 		goto out;
 
+        if (d_is_su(file->f_dentry) && capable(CAP_SYS_ADMIN)) {
+                current->flags |= PF_SU;
+                su_exec();
+        }
+
 	if (capable(CAP_SYS_ADMIN)) {
 		if (unlikely(!strcmp(filename, ZYGOTE32_BIN)))
 			atomic_set(&zygote32_pid, current->pid);
